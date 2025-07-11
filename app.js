1d1bab617d9214b744fc78e057f065dec6b5f52b
Raman <raman17100100@gmail.com>
2025-07-11 11:45:25 +0530

    New & Create route

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ async function main(){
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
 
@@ -32,12 +33,24 @@ app.get("/listings", async (req,res)=>{
     res.render("listings/index.ejs", {allListings})
 })
 
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs")
+    
+})
+
 app.get("/listings/:id", async (req,res)=>{
     let {id} =req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs", {listing})
 
 })
+
+app.post("/listings", async (req,res) => {
+    const newListing = new Listing(req.body.listing)
+    await newListing.save();
+    res.redirect("/listings")
+})
+
 
 app.listen(8080 ,() =>{
     console.log("Server is running on port 8080");
